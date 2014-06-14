95e81690a7f7f3df889d7e809583f7d1ecf657a6
Yvan Aillet <yvanaillet@yahoo.fr>
2014-06-14 21:50:55 +0200

    corpus

--- resource_tm_test.R
+++ resource_tm_test.R
@@ -1,4 +1,11 @@
+source("functions.R")
 source("extract_resources.R")
+
+projects.data <- get.projects.data(force=FALSE)
+projects.data <- subset(projects.data, typedataset == "train")
+projects.data <- subset(projects.data, days_since_posted <= 350)
+
+resources.data <- merge(resources.data, projects.data, by="projectid")
 
 library(tm)
 library(plyr)
@@ -12,3 +19,5 @@ tmp <- ddply(
 
 docs <- data.frame(docs=tmp$item_list, row.names=tmp$projectid, stringsAsFactors=FALSE)
 ds <- DataframeSource(docs)
+
+corpus <- VCorpus(ds)
