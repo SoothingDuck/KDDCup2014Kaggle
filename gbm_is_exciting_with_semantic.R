0aaa1f4be7211feb883e11ae8162ee4c355f99a1
Yvan Aillet <yvanaillet@yahoo.fr>
2014-06-17 15:22:14 +0200

    with semantic

---
+++ gbm_is_exciting_with_semantic.R
@@ -0,0 +1,47 @@
+source("functions.R")
+
+# is.exciting
+model.filename <- file.path("tmp","model_is_exciting.RData")
+
+projects.train.is.exciting.all <- get.projects.data.train(force=FALSE, variable="is_exciting")
+projects.train.is.exciting.all <- subset(projects.train.is.exciting.all, days_since_posted <= 350)
+
+all.cols <- get.all.variables(projects.train.is.exciting.all)
+model.cols <- all.cols
+
+projects.train.is.exciting <- split.train.test(projects.train.is.exciting.all)
+
+model.is.exciting <- get.gbm.model(
+  xtrain=projects.train.is.exciting$train[,all.cols], 
+  ytrain=projects.train.is.exciting$train[,c("is_exciting")], 
+  shrinkage = 0.1,
+  n.trees = 200
+  )
+
+df <- data.frame(summary(model.is.exciting), stringsAsFactor=FALSE)
+important.cols <- as.character(df$var[df$rel.inf > 0.0])
+
+# model on everything
+model.is.exciting.only.refined <- get.gbm.model(
+  xtrain=projects.train.is.exciting.all[,important.cols], 
+  ytrain=projects.train.is.exciting.all[,c("is_exciting")], 
+  shrinkage = 0.1,
+  n.trees = 500)
+
+# valider une soumission
+test.data <- get.projects.data.test(force=FALSE)
+
+prediction <- predict(model.is.exciting.only.refined, newdata=test.data[,important.cols],n.trees=500, type="response")
+
+df <- data.frame(
+  projectid=test.data$projectid,
+  is_exciting=prediction,
+  stringsAsFactors=FALSE
+  )
+
+write.csv(
+  x=df, 
+  file=file.path("submissions","gbm_submission_is_exciting_only_model.csv"),
+  row.names=FALSE,
+  quote=FALSE
+  )
--- generate_item_name_corpus.R
+++ generate_item_name_corpus.R
@@ -20,11 +20,36 @@ tmp <- ddply(
 
 
 docs <- tmp$item_list
-names(docs) <- tmp$projectid
+names(docs) <- as.character(tmp$projectid)
 ds <- VectorSource(docs)
+
 
 print("generation corpus")
 corpus <- VCorpus(ds)
 
-print("ecriture corpus")
-save(corpus, file=file.path("tmp","corpus.RData"))
+print("generatition dtm")
+corpus <- tm_map(corpus, removeNumbers)
+corpus <- tm_map(corpus, removePunctuation)
+# corpus <- tm_map(corpus, toupper)
+corpus <- tm_map(corpus, stripWhitespace)
+corpus <- tm_map(corpus, stemDocument)
+corpus <- tm_map(corpus, removeWords, stopwords("english"))
+
+dtm <- DocumentTermMatrix(corpus,
+                          control=list(stopwords=TRUE))
+
+sparsed.dtm <- removeSparseTerms(dtm, 0.9)
+
+sparsed.dtm.tmp <- inspect(sparsed.dtm)
+sparsed.dtm.tmp <- data.frame(sparsed.dtm.tmp)
+colnames(sparsed.dtm.tmp) <- paste("word", colnames(sparsed.dtm.tmp), sep=".")
+
+for(col in colnames(sparsed.dtm.tmp)) {
+  sparsed.dtm.tmp[, col] <- ifelse(sparsed.dtm.tmp[,col] > 0, 1, 0)
+}
+
+sparsed.dtm.tmp$projectid <- tmp$projectid
+
+semantic.data <- sparsed.dtm.tmp
+
+save(semantic.data, file=file.path("tmp","semantic.RData"))
